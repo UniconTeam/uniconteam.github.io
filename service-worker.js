a38a349af332305dcfcca6289752e94db3c5c271
Viktor Varenik <yavarenikya@gmail.com>
2024-08-18 11:46:31 +0300

    I HATE REACT

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kotleni/ShooterWeb/precache-manifest.217679ab5037cb67b7707473646f7aa7.js"
+  "/precache-manifest.217679ab5037cb67b7707473646f7aa7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kotleni/ShooterWeb/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
